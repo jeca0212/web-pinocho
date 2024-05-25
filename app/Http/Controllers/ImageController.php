30be9e494e3c2c2543f9d3ef0a71b6e83e81c174
unknown <ardila0212@gmail.com>
2024-05-25 12:55:08 +0200

    vendor

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,31 +8,31 @@ use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 class ImageController extends Controller
 {
     public function update(Request $request)
-{
-    try {
-        \Log::info($request->all());
-        $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,webp,svg',
-        ]);
-
-        $imageName = 'principal.' . $request->image->extension();  
-
-        // Carga la imagen en Cloudinary
-        $cloudinary = app('cloudinary');
-        $result = $cloudinary->uploadApi()->upload(
-            $request->file('image')->getRealPath(), 
-            ["public_id" => $imageName]
-        );
-
-        return response()->json([
-            'success' => 'Imagen subida con éxito.',
-            'image' => $imageName
-        ]);
-    } catch (\Exception $e) {
-        \Log::error($e->getMessage());
-        return response()->json(['error' => 'Hubo un error al subir la imagen.'], 500);
+    {
+        try {
+            \Log::info($request->all());
+            $request->validate([
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,webp,svg',
+            ]);
+    
+            $imageName = 'principal.' . $request->image->extension();  
+    
+            // Carga la imagen en Cloudinary
+            $cloudinary = app('cloudinary');
+            $result = $cloudinary->uploadApi()->upload(
+                $request->file('image')->getRealPath(), 
+                ["public_id" => $imageName]
+            );
+    
+            return response()->json([
+                'success' => 'Imagen subida con éxito.',
+                'image' => $imageName
+            ]);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json(['error' => 'Hubo un error al subir la imagen.', 'message' => $e->getMessage()], 500);
+        }
     }
-}
 
     public function upload(Request $request)
     {
