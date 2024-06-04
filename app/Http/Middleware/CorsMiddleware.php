8953e4854bd67b8cd2117d9341bc9c1b55e7f297
unknown <ardila0212@gmail.com>
2024-06-04 11:28:14 +0200

    Add startup script for Railway

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,9 +17,9 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', 'https://restaurantepinochozaragoza.es');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+        //$response->headers->set('Access-Control-Allow-Origin', 'https://restaurantepinochozaragoza.es');
+        //$response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        //$response->headers->set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
 
         return $response;
     }
