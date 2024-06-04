3368a5a2e8ca0fd61adbcbe1fb4e2d1426cfdf5d
unknown <ardila0212@gmail.com>
2024-06-04 12:08:05 +0200

    Add startup script for Railway

--- app/Http/Middleware/OwnCors.php
+++ app/Http/Middleware/OwnCors.php
@@ -16,11 +16,17 @@ class OwnCors
      */
     public function handle(Request $request, Closure $next)
     {
+        header("Access-Control-Allow-Origin: *");
+
         $headers = [
-            'Access-Control-Allow-Origin' => 'https://restaurantepinochozaragoza.es',
             'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
+            
+'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
         ];
+        if ($request->getMethod() == "OPTIONS") {
+            return response('OK')
+                ->withHeaders($headers);
+        }
 
         $response = $next($request);
         foreach ($headers as $key => $value) {
--- startup.sh
+++
@@ -1,3 +0,0 @@
-chmod -R 777 storage/framework
-chmod -R 777 storage/logs
-chmod -R 777 bootstrap/cache
